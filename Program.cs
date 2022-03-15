b22c28b527cb03164a9cf739842bc1c808b0d750
gunayyz <7cgq295@code.edu.az>
2022-03-15 09:30:13 +0400

    15 March

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region fibonacci task
+            Console.Write("Add a number: ");
+            int givenNumber = Convert.ToInt32(Console.ReadLine());
+            int lastDigit = 1;
+            int previousDigit = 0;
+            int firbonacciNumber = 0;
+
+            while(givenNumber>firbonacciNumber)
+            {
+                Console.Write(firbonacciNumber + " ");
+                previousDigit = lastDigit;
+                lastDigit = firbonacciNumber;
+                firbonacciNumber = lastDigit + previousDigit;
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
