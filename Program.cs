a8c10216c0a8ba5b7675a5c6a10277d3e8a081a4
gunayyz <7cgq295@code.edu.az>
2022-03-15 12:17:53 +0400

    Added new files

--- Program.cs
+++ Program.cs
@@ -53,6 +53,56 @@ namespace Classwork
             //}
             //Console.ReadLine();
             #endregion
+            #region Interchange array max and min elements
+            //Console.Write("Add array size: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[] array = new int[size];
+            //for(int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i + 1} element:");
+            //    array[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int minIndex = 0, maxIndex = 0;
+            //for(int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[0] > array[i]) minIndex = i;
+            //    else if(array[0] < array[i]) maxIndex = i;
+            //}
+            //Console.WriteLine();
+            //int temp = array[maxIndex];
+            //array[maxIndex] = array[minIndex];
+            //array[minIndex] = temp;
+            //foreach (int item in array) Console.Write(item + " ");
+            //Console.ReadLine();
+            #endregion
+            #region task2
+            //Console.Write("Add: ");
+            //string given = Console.ReadLine();
+            //string given1 = "";
+            //foreach (char letter in given)
+            //{
+            //    if (letter != 'a') given1 = given1 + letter;
+            //    else if (letter == 'a') given1 = given1 + 'e';
+            //}
+            //given = given1;
+            //Console.WriteLine(given);
+            //Console.ReadLine();
+            #endregion
+            #region task3
+            //Console.Write("Add: ");
+            //string given = Console.ReadLine();
+            //char[] letters = { 'a', 'i', 'o', 'u', 'e' };
+            //int count = 0;
+            //for (int i = 0; i < given.Length; i++)
+            //{
+            //    for (int j = 0; j < letters.Length; j++)
+            //    {
+            //        if (given[i] == letters[j]) count++;
+            //    }
+            //}
+            //Console.WriteLine("Sait seslerin sayi: " + count);
+            //Console.ReadLine();
+            #endregion
         }
     }
 }
