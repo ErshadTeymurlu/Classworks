8239cb8987c14ac66c5b2a1c31744f3c61da47b2
gunayyz <7cgq295@code.edu.az>
2022-03-15 10:04:37 +0400

    15 March Task2 added

--- Program.cs
+++ Program.cs
@@ -11,20 +11,41 @@ namespace Classwork
         static void Main(string[] args)
         {
             #region fibonacci task
-            Console.Write("Add a number: ");
-            int givenNumber = Convert.ToInt32(Console.ReadLine());
-            int lastDigit = 1;
-            int previousDigit = 0;
-            int firbonacciNumber = 0;
+            //Console.Write("Add a number: ");
+            //int givenNumber = Convert.ToInt32(Console.ReadLine());
+            //int lastDigit = 1;
+            //int previousDigit = 0;
+            //int firbonacciNumber = 0;
 
-            while(givenNumber>firbonacciNumber)
-            {
-                Console.Write(firbonacciNumber + " ");
-                previousDigit = lastDigit;
-                lastDigit = firbonacciNumber;
-                firbonacciNumber = lastDigit + previousDigit;
-            }
-            Console.ReadLine();
+            //while(givenNumber>firbonacciNumber)
+            //{
+            //    Console.Write(firbonacciNumber + " ");
+            //    previousDigit = lastDigit;
+            //    lastDigit = firbonacciNumber;
+            //    firbonacciNumber = lastDigit + previousDigit;
+            //}
+            //Console.ReadLine();
+            #endregion
+            #region Bubble Sort
+            //int[] array = new int[] { 1, 6, 2, 78, 6, 8, 7, 10 };
+            //int temp = 0;
+            //for (int i = 0; i < array.Length - 1; i++)
+            //{
+            //    for (int j = 0; j < array.Length - 1; j++)
+            //    {
+            //        if (array[j] > array[j + 1])
+            //        {
+            //            temp = array[j + 1];
+            //            array[j + 1] = array[j];
+            //            array[j] = temp;
+            //        }
+            //    }
+            //}
+            //foreach (int item in array)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.ReadLine();
             #endregion
         }
     }
