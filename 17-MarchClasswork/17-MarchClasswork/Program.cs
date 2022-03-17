21bb616c2307c442a27ffca31c9891e0f4afb1e4
gunayyz <7ahgfnr@code.edu.az>
2022-03-17 10:28:41 +0400

    files added

---
+++ 17-MarchClasswork/17-MarchClasswork/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_MarchClasswork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 7, 4, 23, 6, 3 };
+            int average;
+            Console.Write("Average is " + CalculateAverage(array, out average));
+            Console.ReadLine();
+        }
+        static int CalculateAverage(int[] arr,out int average)
+        {
+            average = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                average = average + arr[i];
+            }
+            average = average / arr.Length;
+            return average;
+            
+        }
+    }
+}
