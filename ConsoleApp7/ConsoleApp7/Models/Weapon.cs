b5ed322a66a2b38c47fd16b2447890c33b04f8a5
gunayyz <7ahgfnr@code.edu.az>
2022-03-28 13:47:39 +0400

    Added new task file

---
+++ ConsoleApp7/ConsoleApp7/Models/Weapon.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7.Models
+{
+    class Weapon
+    {
+        private int _bulletCapacity;
+        private int _bulletAmount;
+        private int _dischargeSecond;
+        private bool _fireMode;
+        public bool FireMode
+        {
+            get { return _fireMode; }
+            set
+            {
+                _fireMode = value;
+            }
+        }
+        public int BulletCapacity
+        {
+            get { return _bulletCapacity; }
+            set
+            {
+                if (value > 0 && value <= 300) _bulletCapacity = value;
+            }
+        }
+        public int BulletAmount
+        {
+            get { return _bulletAmount; }
+            set
+            {
+                if (value <= BulletCapacity && value >= 0) _bulletAmount = value;
+            }
+        }
+        public int DischargeSecond
+        {
+            get { return _dischargeSecond; }
+            set
+            {
+                if (value > 0) _dischargeSecond = value;
+            }
+        }
+        public Weapon(int bulletCapacity, int bulletAmount, int dischargeSecond, char fireMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletAmount = bulletAmount;
+            DischargeSecond = dischargeSecond;
+
+            if(fireMode == 'S' || fireMode == 's') _fireMode = true;
+            if(fireMode == 'A' || fireMode == 'a') _fireMode = false;
+        }
+        public void ChangeFireMode()
+        {
+            if (_fireMode == true)
+            {
+                _fireMode = false;
+                return;
+            }
+            if (_fireMode == false) _fireMode = true;
+        }
+        public void Shot()
+        {
+            if (_fireMode == true)
+            {
+                BulletAmount--;
+                return;
+            }
+            else BulletAmount -= 3;
+        }
+        public double Fire()
+        {
+            double dischargeSecond = (double)(DischargeSecond * BulletAmount) / (double)BulletCapacity;
+            BulletAmount = 0;
+            return dischargeSecond;
+        }
+        public int GetBulletInfo()
+        {
+            return BulletAmount;
+        }
+        public void Reload()
+        {
+            if(BulletAmount!=BulletCapacity) BulletAmount = BulletCapacity;
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp7.Models;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon wp = new Weapon(100, 42, 10,'S');
+            Console.WriteLine("Discharge second: "+wp.Fire());
+            Console.WriteLine("Current Bullet Info: "+wp.GetBulletInfo());
+            wp.Reload();
+            Console.WriteLine("Current Bullet Info: " + wp.GetBulletInfo());
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            Console.WriteLine("SINGLE MODE Current Bullet Info: " + wp.GetBulletInfo());
+            wp.ChangeFireMode(); //auto
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            Console.WriteLine("AUTO MODE Current Bullet Info: " + wp.GetBulletInfo());
+            wp.ChangeFireMode(); //single
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            wp.Shot();
+            Console.WriteLine("SINGLE MODE Current Bullet Info: " + wp.GetBulletInfo());
+            Console.ReadLine();
+        }
+    }
+}
