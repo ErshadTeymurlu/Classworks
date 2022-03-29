4ed48f4d2690133bf6a8d43bec29a1d1c9c77319
Ershad Teymurlu <7ahgfnr@code.edu.az>
2022-03-29 19:37:17 +0400

    fixed a bug, classwork in a date time of 29.03.2022

---
+++ UserTask_/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserTask_.Models
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+        private Regex rgx;
+        public User(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+        public string UserName
+        {
+            get { return _username; }
+            set
+            {
+                rgx = new Regex(@"^\w{6,25}[._]?$");
+                if (rgx.IsMatch(value)) _username = value;
+            }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                bool upperCase = false;
+                bool lowerCase = false;
+                bool isDigit = false;
+                foreach (char letter in value)
+                {
+                    if (!upperCase)
+                    {
+                        if (Char.IsUpper(letter)) upperCase = true;
+                    }
+                    if (!lowerCase)
+                    {
+                        if (Char.IsLower(letter)) lowerCase = true;
+                    }
+                    if (!isDigit)
+                    {
+                        if (Char.IsDigit(letter)) isDigit = true;
+                    }
+                }
+                if (upperCase == true && lowerCase == true && isDigit == true) _password = value;  
+            }
+        }
+    }
+}
---
+++ UserTask_/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserTask_.Models;
+
+namespace UserTask_
+{
+    internal class Programs
+    {
+        static void Main(string[] args)
+        {
+            User us1 = new User("user_name1","sSdsa");
+            Console.WriteLine($"Username - {us1.UserName}\nPassword - {us1.Password}");
+            Console.ReadLine();
+        }
+    }
+}
