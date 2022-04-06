08e5e5d0ea064903b1129a29986984e311b36a6e
gunayyz <7ahgfnr@code.edu.az>
2022-04-06 13:28:03 +0400

    List task

---
+++ 06_04_2022_Classwork/06_04_2022_Classwork/Models/MyList.cs
@@ -0,0 +1,38 @@
+﻿using _06_04_2022_Classwork.Utilies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_04_2022_Classwork.Models
+{
+    class MyList<T>
+    {
+        private T[] _arr;
+        public static void Resize(ref T[] arr,int length)
+        {
+            T[] newArr = new T[length];
+            for (int i = 0; i < arr.Length; i++) newArr[i] = arr[i];
+            arr = newArr;
+        }
+        public void AddItem(T element)
+        {
+            Resize(ref _arr, _arr.Length+1);
+            _arr[_arr.Length-1] = element;
+        }
+        public void AddItems(params T[] elements)
+        {
+            Resize(ref _arr, elements.Length);
+            _arr = elements;
+        }
+        public void Remove(T t)
+        {
+            for(int i = 0; i < _arr.Length; i++)
+            {
+                if (t.Equals(_arr[i])) break;
+                else throw new NotFoundException("");
+            }
+        }
+    }
+}
---
+++ 06_04_2022_Classwork/06_04_2022_Classwork/Program.cs
@@ -0,0 +1,14 @@
+﻿using _06_04_2022_Classwork.Models;
+using System;
+
+namespace _06_04_2022_Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+       
+    }
+}
