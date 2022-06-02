12ba6c125c2c902a083f3a0351369afe6269e491
gunayyz <7ahgfnr@code.edu.az>
2022-06-02 13:42:29 +0400

    Fiorella added

--- task/task2/JS/main.js
+++ task/task2/JS/main.js
@@ -1,7 +1,3 @@
 let inputs = document.querySelectorAll("input")
 let button = document.querySelector("button")
-button.addEventListener('change',()=>{
-    for(let i= 0; i<inputs.length;i++){
-        console.log(inputs[i].value);
-    }
-})           
+
